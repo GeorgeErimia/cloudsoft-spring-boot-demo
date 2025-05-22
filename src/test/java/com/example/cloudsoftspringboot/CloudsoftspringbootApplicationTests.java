9f726126ada9a945b1ba521e2fbcf99ec929fb54
George Erimia <george.erimia1@gmail.com>
2025-05-22 11:46:42 +0300

    initial commit

---
+++ src/test/java/com/example/cloudsoftspringboot/CloudsoftspringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cloudsoftspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudsoftspringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
